4029b96ad31b3d6eab5c644a6a76ddb1bfcc26ca
nahom <nahomtendale@gmail.com>
2024-04-15 23:07:34 -0400

    added tabs to answer view to have STAR_Summary, FullAnswer and Visual_Aid tabs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,39 +5,69 @@ const Card = ({interviewQuestion}) => {
 
     console.log('interviewQuestion: ', interviewQuestion)
     return(
-        <div className="uk-card uk-card-default uk-card-body uk-width-1@m ">
+        <div className="uk-card uk-card-default uk-card-body uk-width-1@m" style={{minHeight: '300px'}}>
             {
                 !showAnswer 
                     ? (
                         <>
                             <h3 className="uk-card-title">{interviewQuestion?.Type}</h3>
-                            <p className='uk-text-normal'>{interviewQuestion?.Question}</p>
-                            <button className="uk-button uk-button-primary" onClick={()=>{setShowAnswer(!showAnswer)}}>Show Answer</button>
+                            <p className='uk-text-normal' style={{maxWidth:'600px', margin:'3em auto'}}>{interviewQuestion?.Question}</p>
+                            <button 
+                                className="uk-button uk-button-primary uk-text-bold" 
+                                onClick={()=>{setShowAnswer(!showAnswer)}}
+                            >
+                                Show Answer
+                            </button>
                         </>
                     )
                     : (
                         <>  
-                            <h2 class="uk-card-title">{interviewQuestion.STAR_Summary.title}</h2>
+                            <div data-uk-switcher="animation: uk-animation-fade; toggle: > *">
+                                <button class="uk-button uk-button-primary" data-type="button">Star Summary</button>
+                                <button class="uk-button uk-button-primary" data-type="button">Full Answer</button>
+                                <button class="uk-button uk-button-primary" data-type="button">Visual Aid</button>
+                            </div>
 
-                            <h3 class="uk-card-title">Situation</h3>
-                            <p>{interviewQuestion.STAR_Summary.Situation}</p>
+                            <ul class="uk-switcher uk-margin">
+                                <li>
+                                    <div>
+                                        <h2 class="uk-card-title" style={{textAlign:'left'}}>{interviewQuestion.STAR_Summary.title}</h2>
 
-                            <h3 class="uk-card-title">Task</h3>
-                            <p>{interviewQuestion.STAR_Summary.Task}</p>
+                                        <h3 class="uk-card-title" style={{textAlign:'left'}}>Situation</h3>
+                                        <p style={{textAlign:'left'}}>{interviewQuestion.STAR_Summary.Situation}</p>
 
-                            <h3 class="uk-card-title">Action</h3>
-                            <ul class="uk-list uk-list-bullet">
-                                {
-                                    interviewQuestion.STAR_Summary.Action.map((action,idx) => {
-                                        return <li key={idx-action}>{action}</li>
-                                    })
-                                }
+                                        <h3 class="uk-card-title" style={{textAlign:'left'}}>Task</h3>
+                                        <p style={{textAlign:'left'}}>{interviewQuestion.STAR_Summary.Task}</p>
+
+                                        <h3 class="uk-card-title" style={{textAlign:'left'}}>Action</h3>
+                                        <ul class="uk-list uk-list-bullet" style={{textAlign:'left'}}>
+                                            {
+                                                interviewQuestion.STAR_Summary.Action.map((action,idx) => {
+                                                    return <li key={idx-action}>{action}</li>
+                                                })
+                                            }
+                                        </ul>
+
+                                        <h3 class="uk-card-title" style={{textAlign:'left'}}>Result</h3>
+                                        <p style={{textAlign:'left'}}>{interviewQuestion.STAR_Summary.Result}</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <p style={{textAlign:'left'}}>{interviewQuestion.FullAnswer}</p>
+                                </li>
+                                <li>
+                                    <p style={{textAlign:'left'}}>{interviewQuestion.Visual_Aid}</p>
+                                </li>
                             </ul>
+                            
 
-                            <h3 class="uk-card-title">Result</h3>
-                            <p>{interviewQuestion.STAR_Summary.result}</p>
-
-                            <button className="uk-button uk-button-primary" onClick={()=>{setShowAnswer(!showAnswer)}}>Hide Answer</button>
+                            <button 
+                                className="uk-button uk-button-primary uk-text-bold" 
+                                style={{marginTop:'3em'}} 
+                                onClick={()=>{setShowAnswer(!showAnswer)}}
+                            >   
+                                Hide Answer
+                            </button>
                         </>
                     )
             }
